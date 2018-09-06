07f809e09e3bdaffcde800519d254e56cc11ce3d
Suheyl Turac <skbsturac@live.com>
2018-09-06 20:11:34 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,34 @@
-//define functions here
 
 $(document).ready(function(){
+  
+  getIt();
+frameIt();
+pressIt();
+submitIt();
+})
 
-// call functions here
+function getIt(){
+  $("p").on("click", function(){
+    window.alert("Hey!");
+    });
+  }
+  
+function frameIt(){
+  $("img").on("load", function(){
+    $("img").addClass("tasty");
+  });
+}
 
-});
+function pressIt(){
+  $("input").on("keydown", function(e){
+    if (e.which === 71){
+      window.alert("G was pressed.");
+    }
+  });
+}
+
+function submitIt(){
+  $("form").on("submit", function(){
+    window.alert("Your form is going to be submitted now.");
+  });
+ }
